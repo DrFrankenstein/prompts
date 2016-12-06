c501d0b7456b9b0b9092da8667411306873a34f1
Carl Tessier <doktorfrankenstein@gmail.com>
2016-12-05 20:40:36 -0500

    aoc: 2016-3: make Microsoft happy.

--- aoc/2016/aoc3.c
+++ aoc/2016/aoc3.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// CL doesn't support static array arguments
+#ifdef _MSC_VER
+# define STATIC_ARRAY_SIZE
+#else
+# define STATIC_ARRAY_SIZE static
+#endif
+
 struct Triangle { unsigned a, b, c; };
 
 static bool triangle_valid(struct Triangle* t)
@@ -11,7 +18,7 @@ static bool triangle_valid(struct Triangle* t)
       && t->c < t->a + t->b;
 }
 
-static bool read_triangles(struct Triangle triangles[static 3])
+static bool read_triangles(struct Triangle triangles[STATIC_ARRAY_SIZE 3])
 {
   int status = scanf("%u %u %u %u %u %u %u %u %u",
       &triangles[0].a, &triangles[1].a, &triangles[2].a,
