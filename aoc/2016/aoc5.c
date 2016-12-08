0878ba637873082a7a3655703088ce2730b4ec71
Carl Tessier <doktorfrankenstein@gmail.com>
2016-12-08 16:18:34 -0500

    aoc2016-5: STAYING ON TARGET with ANIMATION!

--- aoc/2016/aoc5.c
+++ aoc/2016/aoc5.c
@@ -9,6 +9,15 @@
 #include "deps/md5/md5.h"
 #include "deps/md5/md5c.c" // ugly, but I want my AoC solutions to be self-contained. remove this and build accordingly if it prevents you from sleeping at night.
 
+static void output_status(unsigned char hash[static 16], char pass[static 8])
+{
+  putchar('\r');
+  for (size_t i = 0; i < 16; ++i)
+    printf("%02hhx", hash[i]);
+
+  printf(" ... H4XX1NG P455W0RD ... %.8s", pass);
+}
+
 static void hash_string(char* in, size_t len, unsigned char out[static 16])
 {
   MD5_CTX ctx;
@@ -17,26 +26,25 @@ static void hash_string(char* in, size_t len, unsigned char out[static 16])
   MD5Final(out, &ctx);
 }
 
-static void try_gen_char(char* in, size_t len, char pass[static 8])
+static void try_gen_char(char* in, size_t len, char pass[static 8], unsigned idx)
 {
   unsigned char hash[16];
   hash_string(in, len, hash);
   
   unsigned char position = (unsigned char) (hash[2] & 0x0f);
-  if (position < 8 && !pass[position] &&
+  if (position < 8 && pass[position] == '?' &&
       !hash[0] && !hash[1] && !(hash[2] & 0xf0))
   {
-    printf("good hash found. key=%s, hash=", in);
-    for (size_t i = 0; i < 16; ++i)
-      printf("%02hhx", hash[i]);
-    putchar('\n');
-    
     char ch = (char) (hash[3] >> 4);
     if (ch >= 0x0a)
       pass[position] = ch + 'a' - 0xa;
     else
       pass[position] = ch + '0';
+
+    output_status(hash, pass);
   }
+  else if (!(idx & 0x000003ff))
+    output_status(hash, pass);
 }
 
 static void crack_password(char door_id[static 32], size_t idlen, char pass[static 8])
@@ -44,8 +52,8 @@ static void crack_password(char door_id[static 32], size_t idlen, char pass[stat
   char* numstr = door_id + idlen;
   size_t numlen = 31 - idlen;
   
-  memset(pass, 0, 8);
-  for (unsigned idx = 0; idx < UINT_MAX && memchr(pass, 0, 8); ++idx)
+  memset(pass, '?', 8);
+  for (unsigned idx = 0; idx < UINT_MAX && memchr(pass, '?', 8); ++idx)
   {
     int written = snprintf(numstr, numlen + 1, "%u", idx);
     
@@ -61,10 +69,10 @@ static void crack_password(char door_id[static 32], size_t idlen, char pass[stat
       exit(EXIT_FAILURE);
     }
     
-    try_gen_char(door_id, idlen + (size_t) written, pass);
+    try_gen_char(door_id, idlen + (size_t) written, pass, idx);
   }
   
-  if (memchr(pass, 0, 8))
+  if (memchr(pass, '?', 8))
   {
     puts("error: cannot figure out password, ran out of indexes");
     exit(EXIT_FAILURE);
@@ -85,5 +93,5 @@ int main(void)
   char pass[8];
   crack_password(door_id, idlen, pass);
   
-  printf("The password for the security door is: %.8s\n", pass);
+  putchar('\n');
 }
