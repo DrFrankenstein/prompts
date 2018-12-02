8ee41fe3798a475cc61718ea5f7afa2f0f526477
Carl Tessier <doktorfrankenstein@gmail.com>
2018-12-02 03:18:48 -0500

    aoc2018-1: Second star

--- aoc/2018/aoc1.cpp
+++ aoc/2018/aoc1.cpp
@@ -1,15 +1,62 @@
 #include <iterator>
 #include <iostream>
-#include <numeric>
+#include <algorithm>
+#include <set>
+#include <vector>
 
 using namespace std;
 
+class ChronalCalibration
+{
+public:
+    bool isCalibrated() const { return calibrated; }
+
+    template <class It>
+    void applyDrifts(It begin, It end)
+    {
+        all_of(begin, end, [this](int drift) { return applyDrift(drift); });
+
+        if (!frequencyFound)
+        {
+            cout << "Resulting frequency: " << frequency << endl;
+            frequencyFound = true;
+        }
+    }
+
+private:
+    bool applyDrift(int drift)
+    {
+        frequency += drift;
+        checkCalibrated();
+
+        return !frequencyFound || !calibrated;
+    }
+
+    void checkCalibrated()
+    {
+        if (!history.insert(frequency).second && !calibrated)
+        {
+            cout << "Calibrated with frequency " << frequency << endl;
+            calibrated = true;
+        }
+    }
+
+    int frequency { 0 };
+    bool frequencyFound { false };
+    bool calibrated { false };
+    set<int> history { 0 };
+};
+
 int main()
 {
-    // This reads in all numbers from 'cin', separated by whitespace
-    istream_iterator<int> frequencies (cin);
+    istream_iterator<int> input (cin);
+    vector<int> frequencies;
+    copy(input, {}, back_inserter(frequencies));
 
-    int result = accumulate(frequencies, {}, 0);
+    ChronalCalibration calibration;
 
-    cout << "Resulting frequency: " << result << endl;
+    while (!calibration.isCalibrated())
+    {
+        calibration.applyDrifts(begin(frequencies), end(frequencies));
+    }
 }
