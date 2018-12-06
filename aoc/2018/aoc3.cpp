bb477e0dda3dfdd3fa8e96cdf76d468cde69f615
Carl Tessier <doktorfrankenstein@gmail.com>
2018-12-06 00:57:56 -0500

    aoc2018-3: Comments can be nice

--- aoc/2018/aoc3.cpp
+++ aoc/2018/aoc3.cpp
@@ -59,6 +59,7 @@ private:
         return true;
     }
 
+    // id = "#" number
     bool parseId(Claim& claim)
     {
         if (!expect('#'))
@@ -73,6 +74,7 @@ private:
         return true;
     }
 
+    // location = "@" number "," number
     bool parseLocation(Claim& claim)
     {
         if (!expect('@'))
@@ -96,6 +98,7 @@ private:
         return true;
     }
 
+    // size = ":" number "x" number
     bool parseSize(Claim& claim)
     {
         if (!expect(':'))
@@ -122,6 +125,7 @@ private:
     istream& input;
 };
 
+// Extracts and parses a claim from the input file.
 istream& operator >>(istream& input, Claim& claim)
 {
     ClaimParser parser (input);
@@ -135,22 +139,29 @@ template <typename T, unsigned FABRIC_WIDTH, unsigned FABRIC_LENGTH>
 class FabricMapper
 {
 public:
+    // Adds a claim to the map.
     void addClaim(const Claim& claim)
     {
+        // cut the map down to the part that matches the claim
         auto scissors = indices[index_range(claim.y, claim.y + claim.length)][index_range(claim.x, claim.x + claim.width)];
+        // this yields a "view" into the map that acts like another 2D array
         auto piece = map[scissors];
+
+        // mark off all the cells in that view of the map
         bool clean = true;
         for (auto row : piece)
             for (T& cell : row)
                 if (!claimCell(claim, cell))
                     clean = false;
 
+        // if we didn't hit another claim, mark it clean (for now)
         if (clean)
             cleanClaims.insert(claim.id);
     }
 
     long long conflicts() const
-    {
+    {  
+        // count all the map cells that are marked as conflicting (-1)
         return accumulate(cbegin(map), cend(map), 0LL, [](auto count, auto row) {
             return count + count_if(cbegin(row), cend(row), [](T val) { return val == -1; });
         });
@@ -168,6 +179,7 @@ public:
         if (size > 1)
             cerr << "warning: several non-conflicting claims." << endl;
 
+        // we're only supposed to have one, so just return the first ID in the set
         return *cbegin(cleanClaims);
     }
 
@@ -175,15 +187,17 @@ private:
     bool claimCell(const Claim& claim, T& cell)
     {
         if (cell == 0)
-        {
+        {   // no conflict, just reserve it with the claim ID
             cell = claim.id;
             return true;
         }
 
+        // that cell's already taken, remove this claim ID from the clean set if it's in there
         auto elem = cleanClaims.find(cell);
         if (elem != end(cleanClaims))
             cleanClaims.erase(elem);
 
+        // mark the cell as conflicting
         cell = -1;
 
         return false;
@@ -197,8 +211,11 @@ int main()
 {
     auto start = chrono::high_resolution_clock::now();
 
+    // the story says "at least 1000 inches" but a glance through the input file seems to
+    // place all the claims within 1000x1000; guess it'll crash if we're wrong
     FabricMapper<int, 1000, 1000> mapper;
 
+    // go through all the claims in the standard input (cin) and add them to the map
     istream_iterator<Claim> claims (cin);
     for_each(claims, {}, [&](const Claim& claim) { mapper.addClaim(claim); });
 
