f95b99ad01e5ab388d47f840c618b442bdb6c566
Carl Tessier <doktorfrankenstein@gmail.com>
2019-06-13 21:09:56 -0400

    Port 100_doors to K&R C.

--- rosettacode/100_doors.c
+++ rosettacode/100_doors.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 typedef unsigned char uint8_t;
 
-void print_doors(uint8_t doors[])
+void print_doors(doors)
+	  uint8_t doors[];
 {
     uint8_t door;
     for (door = 0; door < 100; door++)
         printf("Door #%u: %s\n", (unsigned) door + 1, doors[door]? "open" : "closed");
 }
 
-void doors(void)
+void doors()
 {
-    uint8_t doors[100] = {0},
+    uint8_t doors[100],
         pass, door;
+    memset(doors, 0, sizeof doors);
     for (pass = 1; pass <= 100; pass++)
         for (door = 0; door < 100; door += pass)
             doors[door] = !doors[door];
@@ -21,20 +22,21 @@ void doors(void)
     print_doors(doors);
 }
 
-void doors_opt(void)
+void doors_opt()
 {
-    uint8_t doors[100] = {0},
+    uint8_t doors[100],
         factor, door = 0;
+    memset(doors, 0, sizeof doors);
     for(factor = 1; (door += (factor * 2) - 1) < 100; factor++)
         doors[door] = !doors[door];
 
     print_doors(doors);
 }
 
-int main(void)
+int main()
 {
     doors();
     doors_opt();
 
-    return EXIT_SUCCESS;
+    return 0;
 }
