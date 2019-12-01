552ea79f99f900b40d2e917dc9396bc723d6ce31
Carl Tessier <doktorfrankenstein@gmail.com>
2019-12-01 17:25:29 -0500

    [aoc2019] Problem 1 star 2

--- aoc/2019/aoc1.cob
+++ aoc/2019/aoc1.cob
@@ -5,24 +5,25 @@
            input-output section.
              file-control.
                select input-file assign to 'input.txt'
-                   organization is line sequential
-                   file status is file-status.
+                 organization is line sequential
+                 file status is file-status.
 
        data division.
            file section.
            fd input-file.
-           01 mass     pic X(6).
+           01 mass        pic X(6).
 
            working-storage section.
            77 file-status pic 99.
-           77 total-fuel pic 9(8) value 0.
-           77 curr-fuel pic 9(6).
+           77 total-fuel  pic 9(8) value 0.
+           77 curr-mass   pic S9(6).
+           77 curr-fuel   pic S9(6).
 
        procedure division.
        0000-main.
            open input input-file.
            perform 0010-calculate-fuel
-               until file-status equal to 10.                           (end of file)
+             until file-status equal to 10.                             (end of file)
            close input-file.
            
            display 'Total fuel needed: ' total-fuel.
@@ -30,10 +31,22 @@
        
        0010-calculate-fuel.
            read input-file
-               at end exit paragraph.
+             at end exit paragraph.
+
+           move function numval(mass) to curr-mass.
+      *    display 'mass ' curr-mass.
            
-           compute curr-fuel = function numval(mass) / 3 - 2.
+           perform until curr-mass is <= 0
+               compute curr-fuel = curr-mass / 3 - 2
 
-      *    display 'mass ' mass ' fuel ' curr-fuel ' total ' total-fuel.
+               if curr-fuel is > 0 then
+                  add curr-fuel to total-fuel
+               end-if
+               
+               move curr-fuel to curr-mass
 
-           add curr-fuel to total-fuel.
+      *        display 
+      *          ' fuel ' curr-fuel
+      *          ' total ' total-fuel
+      *        end-display
+           end-perform.
