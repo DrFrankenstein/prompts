d1f94233a6ab5919d6ae10c605c1e5273078f5fa
Carl Tessier <doktorfrankenstein@gmail.com>
2019-12-09 23:53:21 -0500

    [aoc2019-5] WIP

--- aoc/2019/aoc2.c
+++ aoc/2019/aoc2.c
@@ -3,9 +3,25 @@
 #include <stdbool.h>
 #include <string.h>
 
-typedef unsigned int IntCode;                 // the type of the elements in the machine's memory; in other words, an IntCode "byte"
-#define IC "u"                                // the printf/scanf specifier that goes with IntCode
-enum Opcode { ADD = 1, MUL = 2, HALT = 99 };  // names for the IntCode opcodes
+typedef int IntCode;  // the type of the elements in the machine's memory; in other words, an IntCode "byte"
+#define IC "d"        // the printf/scanf specifier that goes with IntCode
+
+enum Opcode
+{	// names for the IntCode opcodes
+	ADD = 1,
+	MUL = 2,
+	IN = 3,
+	OUT = 4,
+	HALT = 99
+};
+
+struct Computer
+{
+	size_t ip;
+	size_t memsize;
+	IntCode* mem;
+	bool running;
+};
 
 // reads one integer from 'image', followed by a comma or EOF.
 static IntCode read_integer(FILE* image)
@@ -79,63 +95,105 @@ static IntCode* read_image(FILE* image, size_t* sizeref)
 }
 
 // the 'add' opcode
-static void add(IntCode* mem, size_t ip)
+static void add(struct Computer* computer)
 {
-	IntCode p1 = mem[ip + 1], 
-		p2 = mem[ip + 2], 
-		to = mem[ip + 3];
+	IntCode p1 = computer->mem[computer->ip + 1],
+		p2 = computer->mem[computer->ip + 2],
+		to = computer->mem[computer->ip + 3];
 	//printf("%08zu ADD %"IC"(%"IC"),%"IC"(%"IC"),%"IC"\n",
 	//	ip, p1, mem[p1], p2, mem[p2], to);
 
-	mem[to] = mem[p1] + mem[p2];
+	computer->mem[to] = computer->mem[p1] + computer->mem[p2];
+	computer->ip += 4;
 }
 
 // the 'multiply' opcode
-static void mul(IntCode* mem, size_t ip)
+static void mul(struct Computer* computer)
 {
-	IntCode p1 = mem[ip + 1], 
-		p2 = mem[ip + 2], 
-		to = mem[ip + 3];
+	IntCode p1 = computer->mem[computer->ip + 1],
+		p2 = computer->mem[computer->ip + 2],
+		to = computer->mem[computer->ip + 3];
 	//printf("%08zu MUL %"IC"(%"IC"),%"IC"(%"IC"),%"IC"\n",
 	//	ip, p1, mem[p1], p2, mem[p2], to);
 
-	mem[to] = mem[p1] * mem[p2];
+	computer->mem[to] = computer->mem[p1] * computer->mem[p2];
+	computer->ip += 4;
+}
+
+// the 'input' opcode
+static void in(struct Computer* computer)
+{
+	printf("Input requested at %zu: ", computer->ip);
+	IntCode input;
+	int status;
+	do
+	{
+		status = scanf(" %"IC, &input);
+	} while (status < 1);
+
+	IntCode p1 = computer->mem[computer->ip + 1];
+	computer->mem[p1] = input;
+
+	computer->ip += 2;
+}
+
+static void out(struct Computer* computer)
+{
+	IntCode p1 = computer->mem[computer->ip + 1];
+	printf("(%08zu) %"IC"\n", computer->ip, computer->mem[p1]);
+
+	computer->ip += 2;
+}
+
+static void halt(struct Computer* computer)
+{
+	computer->running = false;
 }
 
 // runs one instruction; returns false on halt
-static bool step(IntCode* mem, size_t ip)
+static void step(struct Computer* computer)
 {
-	IntCode code = mem[ip];
+	IntCode code = computer->mem[computer->ip];
 	switch (code)
 	{
 	case ADD:
-		add(mem, ip);
-		return true;
+		add(computer);
+		break;
 
 	case MUL:
-		mul(mem, ip);
-		return true;
+		mul(computer);
+		break;
+
+	case IN:
+		in(computer);
+		break;
+
+	case OUT:
+		out(computer);
+		break;
 
 	case HALT:
-	//	printf("%08zu HALT\n", ip);
-		return false;
+		halt(computer);
+		//	printf("%08zu HALT\n", ip);
+		break;
 
 	default:
-		printf("%08zu ??? (%"IC")\n", ip, code);	// invalid instruction
+		printf("%08zu ??? (%"IC")\n", computer->ip, code);	// invalid instruction
 		exit(1);
 	}
 }
 
 // runs the IntCode machine until halt or memory overrun
-static void run(IntCode* mem, size_t size)
+static void run(struct Computer* computer)
 {
-	size_t ip = 0;
-	while (step(mem, ip))
+	computer->ip = 0;
+	computer->running = true;
+	while (computer->running)
 	{
-		ip += 4;
-		if (ip >= size)
+		step(computer);
+		if (computer->ip >= computer->memsize)
 		{
-			fprintf(stderr, "%08zu !!! out of bounds\n", ip);
+			fprintf(stderr, "%08zu !!! out of bounds\n", computer->ip);
 			exit(1);
 		}
 	}
@@ -144,34 +202,36 @@ static void run(IntCode* mem, size_t size)
 // creates and runs an IntCode machine with 'noun' and 'verb' params set
 static IntCode run_with_params(IntCode* initmem, size_t size, IntCode noun, IntCode verb)
 {	// we create a copy of the memory so that we don't clobber anything between reruns
-	IntCode* mem = calloc(size, sizeof *initmem);
+	IntCode* mem = calloc(size, sizeof * initmem);
 	if (!mem)
 	{
 		perror("cannot create an IntCode computer");
 		exit(1);
 	}
 
-	memcpy(mem, initmem, size * sizeof *mem);
+	memcpy(mem, initmem, size * sizeof * mem);
 	mem[1] = noun;
 	mem[2] = verb;
 
-	run(mem, size);
+	struct Computer computer;
+	computer.mem = mem;
+	computer.memsize = size;
+
+	run(&computer);
 
 	IntCode result = mem[0];
 	free(mem);
 	return result;
 }
 
-int main(void)
+// finds the params that result in 'target' (Day 2 part 2)
+int find_params(IntCode* initmem, size_t size, IntCode target)
 {
-	size_t size;
-	IntCode* initmem = read_image(stdin, &size);
-
 	for (IntCode noun = 0; noun < 100; ++noun)
 		for (IntCode verb = 0; verb < 100; ++verb)
 		{
 			IntCode result = run_with_params(initmem, size, noun, verb);
-			if (result == 19690720)
+			if (result == target)
 			{
 				printf("noun = %"IC", verb = %"IC", result = %"IC"\n", noun, verb, 100 * noun + verb);
 				return 0;
@@ -180,4 +240,27 @@ int main(void)
 
 	puts("not found");
 	return 1;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		fprintf(stderr, "usage: %s <image-file>\n", argv[0]);
+		return 1;
+	}
+
+	FILE* image = fopen(argv[1], "r");
+
+	size_t size;
+	IntCode* initmem = read_image(image, &size);
+
+	//return find_params(initmem, size, 19690720);
+
+	struct Computer computer;
+	computer.mem = initmem;
+	computer.memsize = size;
+	run(&computer);
+
+	return 0;
 }
