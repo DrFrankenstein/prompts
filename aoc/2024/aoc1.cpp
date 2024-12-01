ee5d825ffce446f472386e0f63b6cd6ce7eaaf0a
Carl Tessier <doktorfrankenstein@gmail.com>
2024-12-01 11:11:11 -0500

    [aoc2024-1] First star, C++

---
+++ aoc/2024/aoc1.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <ranges>
+#include <utility>
+#include <vector>
+
+using std::abs, std::cin, std::cout, std::ranges::fold_left, std::istream,
+	std::move, std::pair, std::plus, std::ranges::sort, std::vector,
+	std::views::zip_transform;
+
+template <typename T>
+static auto readLists(istream& input) -> pair<vector<T>, vector<T>>
+{
+	vector<T> leftList, rightList;
+	while (input)
+	{
+		T left, right;
+		cin >> left >> right;
+
+		if (!input) break;
+
+		leftList.push_back(left);
+		rightList.push_back(right);
+	}
+
+	return pair{ move(leftList), move(rightList) };
+}
+
+int main()
+{
+	auto [leftList, rightList] = readLists<int>(cin);
+	sort(leftList);
+	sort(rightList);
+
+	auto const distance = [](int left, int right) { return abs(left - right); };
+
+	auto const distances = zip_transform(distance, leftList, rightList);
+	auto const sum = fold_left(distances, 0, plus<int>{});
+
+	cout << sum;
+}
