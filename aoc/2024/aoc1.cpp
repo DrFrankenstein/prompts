ab613365418447e6c05317527228d48d443a10da
Carl Tessier <doktorfrankenstein@gmail.com>
2024-12-01 11:33:18 -0500

    [aoc2024-1] Second star, C++

--- aoc/2024/aoc1.cpp
+++ aoc/2024/aoc1.cpp
@@ -3,12 +3,14 @@
 #include <functional>
 #include <iostream>
 #include <ranges>
+#include <tuple>
 #include <utility>
 #include <vector>
 
-using std::abs, std::cin, std::cout, std::ranges::fold_left, std::istream,
-	std::move, std::pair, std::plus, std::ranges::sort, std::vector,
-	std::views::zip_transform;
+using std::abs, std::views::cartesian_product, std::cin, std::cout,
+	std::ranges::fold_left, std::istream, std::move, std::pair, std::plus,
+	std::ranges::sort, std::views::transform, std::vector,
+	std::views::zip_transform, std::tuple;
 
 template <typename T>
 static auto readLists(istream& input)
@@ -35,11 +37,23 @@ static auto totalDistance(vector<int>& leftList, vector<int>& rightList)
 	return fold_left(distances, 0, plus<int>{});
 }
 
+static auto similarityScore(vector<int>& leftList, vector<int>& rightList)
+{
+	auto matches = cartesian_product(leftList, rightList)
+		| transform([](tuple<int, int> values) {
+			auto [left, right] = values;
+			return left == right ? left : 0;
+		});
+
+	return fold_left(matches, 0, plus<int>{});
+}
+
 int main()
 {
 	auto [leftList, rightList] = readLists<int>(cin);
 	sort(leftList);
 	sort(rightList);
 
-	cout << totalDistance(leftList, rightList);
+	cout << "Total distance: " << totalDistance(leftList, rightList) << '\n';
+	cout << "Similarity score: " << similarityScore(leftList, rightList) << '\n';
 }
